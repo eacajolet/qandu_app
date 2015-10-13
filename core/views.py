1f26db252867815d75574b7baff3db24ea218dff
Eric <eacajolet@quinnipiac.edu>
2015-10-13 02:12:01 +0000

    Prevent users from submitting multiple answers

--- core/views.py
+++ core/views.py
@@ -31,6 +31,8 @@ class QuestionDetailView(DetailView):
         question = Question.objects.get(id=self.kwargs['pk'])
         answers = Answer.objects.filter(question=question)
         context['answers'] = answers
+        user_answers = Answer.objects.filter(question=question, user=self.request.user)
+        context['user_answers'] = user_answers
         return context
 
 class QuestionUpdateView(UpdateView):
@@ -64,6 +66,9 @@ class AnswerCreateView(CreateView):
         return self.object.question.get_absolute_url()
 
     def form_valid(self, form):
+        question = Question.objects.get(id=self.kwargs['pk'])
+        if Answer.objects.filter(question=question, user=self.request.user).exists():
+            raise PermissionDenied()
         form.instance.user = self.request.user
         form.instance.question = Question.objects.get(id=self.kwargs['pk'])
         return super(AnswerCreateView, self).form_valid(form)
@@ -76,7 +81,7 @@ class AnswerUpdateView(UpdateView):
 
     def get_success_url(self):
         return self.object.question.get_absolute_url()
-      
+
     def get_object(self, *args, **kwargs):
             object = super(AnswerUpdateView, self).get_object(*args, **kwargs)
             if object.user != self.request.user:
@@ -90,7 +95,7 @@ class AnswerDeleteView(DeleteView):
 
     def get_success_url(self):
         return self.object.question.get_absolute_url()
-      
+
     def get_object(self, *args, **kwargs):
             object = super(AnswerDeleteView, self).get_object(*args, **kwargs)
             if object.user != self.request.user:
